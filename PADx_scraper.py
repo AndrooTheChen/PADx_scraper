721fda458a6c97af6a60ca2107badf5a2c68e985
Andrew Chen <achen226@gmail.com>
2020-04-09 17:33:08 -0500

    fixed parser, scraper works for standard drops

--- PADx_scraper.py
+++ PADx_scraper.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
+
+def remove_non_ascii_1(text):
+    """
+    Remove extended ASCII characters resulting from trying to
+    translate JP names to ENG
+    """
+    return ''.join(i for i in text if ord(i)<128)
+
 # Get URL to all fodder and 5* gods
 url_rem = "http://puzzledragonx.com/en/monsterbook.asp?r=3,4,5&s=47,48,53,37,107,29,94,81,8,13,44,9,11,80,24,98,14,68,12,66,10,30,31,43,33,86,27,82,104&b1=1#view"
 
@@ -21,22 +29,29 @@ f_rem_urls = open("rem_urls.txt", "w")
 
 pdx_url = "http://puzzledragonx.com/en/img/monster/MONS_"
 
+# Traverse HTML of all monsters queried
 for link in rem_soup.find_all("img", {"class": "onload"}):
     mon_id = link.get('data-original')
     if mon_id is None:
         continue
-    # mon = rem_soup.find(class_="indexframe").extract()
-    # f_rem_urls.write(f"{href} : {mon}\n")
+
     mon_name_struct = link.get('title').split()
 
     # Gets Monster name : URL but cuts some out due to .index()
-    # last_idx = mon_name_struct.index('Active:')
-    # mon_name_str = mon_name_struct[1:last_idx-1]
-    # mon_name = ' '.join(mon_name_str)
-    # mon_nbr = mon_id[14:].strip('.png')
-    # mon_url = f"{pdx_url}{mon_nbr}.jpg"
-    
-    # f_rem_urls.write(f"{mon_name}: {mon_url}\n") 
-    f_rem_urls.write(f"{mon_id}\n")
+    if ('Active:' not in mon_name_struct):
+        # Edge case for monster Manekimewdra which doesn't have an Active skill
+        mon_name = mon_name_struct[1]
+    else:
+        # Filter out name of monster
+        last_idx = mon_name_struct.index('Active:')
+        mon_name_str = mon_name_struct[1:last_idx-1]
+        mon_name_unfiltered = ' '.join(mon_name_str)
+        mon_name = remove_non_ascii_1(mon_name_unfiltered).rstrip()
+
+    # Build URL
+    mon_nbr = mon_id[14:].strip('.png')
+    mon_url = f"{pdx_url}{mon_nbr}.jpg"
+        
+    f_rem_urls.write(f"{mon_name}: {mon_url}\n") 
 
 f_rem_urls.close()
